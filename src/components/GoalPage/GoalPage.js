a72487ecd5f09e43483659e39022f6e146a300f2
Hailee Miu <hailee.miu@gmail.com>
2018-12-24 10:11:53 -0500

    fix formatting of GoalPage

--- src/components/GoalPage/GoalPage.js
+++ src/components/GoalPage/GoalPage.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class GoalPage extends Component {
-
   /* Global Variables */
   state = {
-    title: ''
+    title: '',
   }
 
   /* Life Cycle Events */
@@ -17,28 +16,40 @@ class GoalPage extends Component {
 
   // Save the input
   handleChangeTitle = (event) => {
-    this.setState({title: event.target.value})
+    this.setState({ title: event.target.value });
   }
 
   // Send the input to the router query
   handleSubmit = (event) => {
     event.preventDefault();
-    axios.post('/api/goal', {title: this.state.title})
-      .then(res => {
-        // WIP
-        console.log(res.data);
-      })
+    axios.post('/api/goal', { title: this.state.title })
+      .then(this.handleSubmitSuccess)
+      .catch(this.handleSubmitError);
   }
+
+  // handleSubmit goal Success
+  handleSubmitSuccess = response => console.log(response.data); // WIP
+
+  // handleSubmit goal Error
+  handleSubmitError = error => console.log('Error in adding goal:', error);
+
 
   // Retrieve goals
   getGoals = () => {
     axios.get('/api/goal')
-      .then(res => {
-        console.log(res);
-      })
+      .then(this.getGoalsSuccess)
+      .catch(this.getGoalsError);
   }
 
-  /* Render Page Content*/
+  // Retrieve goals Success
+  getGoalsSuccess = (response) => { console.log(response.data) }
+
+  // Retrieve goals Error
+  getGoalsError = (error) => {
+    console.log('Error in retrieving goals from server:', error);
+  }
+
+  /* Render Page Content */
 
   render() {
     return (
@@ -46,19 +57,16 @@ class GoalPage extends Component {
         <h1>Goal Page</h1>
         {/* Add Goal */}
         <form onSubmit={this.handleSubmit}>
-          <input type='text' onChange={this.handleChangeTitle}></input>
-          <input type='submit'></input>
+          <input type="text" onChange={this.handleChangeTitle} />
+          <input type="submit" />
         </form>
 
         {/* View Goals */}
-        <ul>
-          {/* <input type='radio'></input> */}
-          <li>test</li>
-        </ul>
+        {/* <input type='radio'></input> */}
+        <ul></ul>
       </div>
-    )
+    );
   }
-
 }
 
 export default GoalPage;
