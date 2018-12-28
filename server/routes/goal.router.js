5a771d4299c3aaedf46feb6335712c274636e8af
Hailee Miu <hailee.miu@gmail.com>
2018-12-28 13:22:39 -0600

    update code to fit new database tables

--- server/routes/goal.router.js
+++ server/routes/goal.router.js
@@ -5,11 +5,12 @@ const router = express.Router();
 
 // Handles POST request for new goal item
 router.post('/', (req, res) => {
-  const { name } = req.body;
+  const { goal } = req.body;
+  const { instancesPerWeek } = req.body;
 
-  const queryText = 'INSERT INTO goals (user_id, name) VALUES ($1, $2) RETURNING id;';
+  const queryText = 'INSERT INTO goals (user_id, goal, instances_per_week) VALUES ($1, $2, $3) RETURNING id;';
 
-  pool.query(queryText, [req.user.id, name])
+  pool.query(queryText, [req.user.id, goal, instancesPerWeek])
     .then(() => { res.sendStatus(201); })
     .catch((error) => {
       console.log('Error in posting goal:', error);
@@ -19,7 +20,7 @@ router.post('/', (req, res) => {
 
 // Handles retrieving goal list
 router.get('/', (req, res) => {
-  const queryText = 'SELECT goals.id, goals.user_id, goals.name FROM person LEFT JOIN goals ON person.id = goals.user_id WHERE goal.user_id = $1;';
+  const queryText = 'SELECT goals.id, goals.user_id, goals.goal, instances_per_week FROM person LEFT JOIN goals ON person.id = goals.user_id WHERE goals.user_id = $1;';
 
   pool.query(queryText, [req.user.id])
     .then((results) => { res.send(results.rows); })
