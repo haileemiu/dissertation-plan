8bc6b2f731187bb4f2a5c5f5c7f4bde541973e20
Hailee Miu <hailee.miu@gmail.com>
2019-01-03 10:12:02 -0600

    add static default data

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -21,15 +21,63 @@ router.post('/register', async (req, res) => {
     // Registers user
     const queryText = 'INSERT INTO person (email, username, password) VALUES ($1, $2, $3) RETURNING id';
 
-    // TODO: add real section headings
     const results = await pool.query(queryText, [email, username, encryptedPassword]);
 
     // Creates the default dissertation_plan data for the new user
     // No need for data sterilization because this is not based on user input
-    const insertResult = await pool.query(`INSERT INTO dissertation_sections (user_id, name) VALUES (${results.rows[0].id}, 'Temporary Section 1'), (${results.rows[0].id}, 'Temporary Section 2') RETURNING id;`);
+    const insertResult = await pool.query(`
+        INSERT INTO dissertation_sections (user_id, name) VALUES 
+        (${results.rows[0].id}, 'During Course Work'),
+        (${results.rows[0].id}, 'Proposal Development'), 
+        (${results.rows[0].id}, 'Proposal Defense'),
+        (${results.rows[0].id}, 'Conducting Research'),  
+        (${results.rows[0].id}, 'Dissertation Writing'),
+        (${results.rows[0].id}, 'Dissertation Defense')
+        RETURNING id;`);
 
     // Creates the default dissertation_steps
-    await pool.query(`INSERT INTO dissertation_steps (name, section_id) VALUES ('temp step 1a', ${insertResult.rows[0].id}), ('temp step 1b', ${insertResult.rows[0].id}), ('temp step 2a', ${insertResult.rows[1].id});`);
+    await pool.query(`
+      INSERT INTO dissertation_steps (name, section_id) VALUES
+      ('Become acquainted with library and librarian', ${insertResult.rows[0].id}),
+      ('Become acquainted with issues and literature in the field', ${insertResult.rows[0].id}),
+      ('Search for dissertation topic', ${insertResult.rows[0].id}),
+      ('Search for dissertation director', ${insertResult.rows[0].id}),
+      ('Choose a topic', ${insertResult.rows[0].id}),
+      ('Choose a director', ${insertResult.rows[0].id}),
+      ('Choose other committee members', ${insertResult.rows[0].id}),
+
+      ('Develop a topic', ${insertResult.rows[1].id}),
+      ('Consider grant or loan application if necessary', ${insertResult.rows[1].id}),
+      ('Begin meeting with director to discuss proposal', ${insertResult.rows[1].id}),
+      ('Find out requirements for proposal submission', ${insertResult.rows[1].id}),
+      ('Outline proposal', ${insertResult.rows[1].id}),
+      ('Complete detailed review of literature; construct theoretical framework; write review of literature chapter', ${insertResult.rows[1].id}),
+      ('Decide on methodology; write method chapter', ${insertResult.rows[1].id}),
+      ('Give proposal to committee', ${insertResult.rows[1].id}),
+
+      ('File paperwork and schedule defense', ${insertResult.rows[2].id}),
+      ('Arrange permission, community contracts, and other preliminaries', ${insertResult.rows[2].id}),
+      ('Defend proposal', ${insertResult.rows[2].id}),
+      ('Revise proposal if necessary', ${insertResult.rows[2].id}),
+
+      ('Meet with director to draw up schedule for research', ${insertResult.rows[3].id}),
+      ('Conduct research for study', ${insertResult.rows[3].id}),
+      ('Analyze data from research', ${insertResult.rows[3].id}),
+
+      ('Outline dissertation', ${insertResult.rows[4].id}),
+      ('Meet with director to discuss outline and preliminary data analysis', ${insertResult.rows[4].id}),
+      ('Update proposal chapters for dissertation', ${insertResult.rows[4].id}),
+      ('Write results chapter', ${insertResult.rows[4].id}),
+      ('Write summary and conclusions chapter ', ${insertResult.rows[4].id}),
+      ('Find out requirements for submission and scheduling of final oral defense', ${insertResult.rows[4].id}),
+      ('Polish writing; consult with editor if necessary', ${insertResult.rows[4].id}),
+      ('Submit draft of dissertation to committee', ${insertResult.rows[4].id}),
+      
+      ('Submit dissertation paperwork and schedule for oral defense', ${insertResult.rows[5].id}),
+      ('Have dissertation professionally typed or printed from word processor', ${insertResult.rows[5].id}),
+      ('Defend dissertation', ${insertResult.rows[5].id}),
+      ('Make minor revisions, if necessary', ${insertResult.rows[5].id}),
+      ('Graduate', ${insertResult.rows[5].id});`);
 
     res.sendStatus(201);
   } catch (err) {
