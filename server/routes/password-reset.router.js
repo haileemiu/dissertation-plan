c8cc44cb06d46c9272e436fa45e05b931200b6c9
Hailee Miu <hailee.miu@gmail.com>
2019-01-14 09:55:01 -0600

    set up query with basic alerts to check for email existence

--- server/routes/password-reset.router.js
+++ server/routes/password-reset.router.js
@@ -4,5 +4,18 @@ const pool = require('../modules/pool');
 
 const router = express.Router();
 
+const queryEmailExists = 'SELECT EXISTS (SELECT person.email FROM person WHERE email = $1);';
+
+router.get('/', (req, res) => {
+  pool.query(queryEmailExists, [req.query.email])
+    .then((response) => {
+      console.log(response.rows);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
--- src/components/LoginPage/PasswordReset.js
+++ src/components/LoginPage/PasswordReset.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PasswordReset extends Component {
   state = {
     email: '',
   }
 
+  // Holds input value
   inputEmail = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   }
 
+  // 1) Checks if email exists
   submitEmail = (event) => {
     event.preventDefault();
-    console.log('submitted', this.state.email);
+    axios.get(`/api/password-reset?email=${this.state.email}`)
+      .then(this.submitEmailSuccess)
+      .catch(this.submitEmailError);
+  }
+
+  submitEmailSuccess = () => {
+    this.setState({ email: '' });
+    alert('If your email is in our database, you will get a password reset link in your inbox');
+  };
+
+  submitEmailError = () => {
+    alert('Something went wrong');
   }
 
   render() {
