d9a2ca819eaa0182ad5054e71bbf30d2b917ca30
Hailee Miu <hailee.miu@gmail.com>
2019-01-25 15:48:55 -0600

    build

---
+++ build/precache-manifest.f52339bb521bb2c3066f186bf3cf0c9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6013a5a066f147f2f31aedf95f4e9e6b",
+    "url": "https://taina-app.herokuapp.com/static/media/taina_logo.6013a5a0.png"
+  },
+  {
+    "revision": "06ea20d2f30900e5ac1c5f3b424665cf",
+    "url": "https://taina-app.herokuapp.com/static/media/banner-goals-1920.06ea20d2.jpg"
+  },
+  {
+    "revision": "baa0a61cd174336afab1c370f33549c7",
+    "url": "https://taina-app.herokuapp.com/static/media/banner-displan-1920.baa0a61c.jpg"
+  },
+  {
+    "revision": "1e6e6fdffc5a9774cf61",
+    "url": "https://taina-app.herokuapp.com/static/js/runtime~main.1e6e6fdf.js"
+  },
+  {
+    "revision": "26db8aedaf1b9fa40d6b",
+    "url": "https://taina-app.herokuapp.com/static/js/main.26db8aed.chunk.js"
+  },
+  {
+    "revision": "605e3ca5397620e9c889",
+    "url": "https://taina-app.herokuapp.com/static/js/1.605e3ca5.chunk.js"
+  },
+  {
+    "revision": "26db8aedaf1b9fa40d6b",
+    "url": "https://taina-app.herokuapp.com/static/css/main.b89112b2.chunk.css"
+  },
+  {
+    "revision": "442928a8182cece9ed5a70897e80eaf6",
+    "url": "https://taina-app.herokuapp.com/index.html"
+  }
+];
