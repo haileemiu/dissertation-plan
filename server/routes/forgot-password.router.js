d2b395999d592ffeb1d3e2c032e4dd15b6ab5c5e
Hailee Miu <hailee.miu@gmail.com>
2019-01-28 10:41:47 -0600

    chagne package json homepage for heroku, rebuild

--- server/routes/forgot-password.router.js
+++ server/routes/forgot-password.router.js
@@ -1,9 +1,10 @@
 // Path: /api/forgot-password
 const express = require('express');
 const nodemailer = require('nodemailer');
-const pool = require('../modules/pool');
-const encryptLib = require('../modules/encryption');
 const uuidv1 = require('uuid/v1');
+const pool = require('../modules/pool');
+// const encryptLib = require('../modules/encryption');
+
 
 
 const router = express.Router();
