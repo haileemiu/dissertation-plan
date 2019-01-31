8304bd38385d6b7ab2cb83925441f916d684840f
Hailee Miu <hailee.miu@gmail.com>
2019-01-31 09:11:53 -0600

    add contact form component

--- src/components/CoachingContact/CoachingContactPage.js
+++ src/components/CoachingContact/CoachingContactPage.js
@@ -4,6 +4,7 @@ import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { withStyles, createMuiTheme } from '@material-ui/core/styles';
 import { teal, red } from '@material-ui/core/colors';
 import CoachingBanner from './CoachingBanner';
+import ContactForm from './ContactForm';
 
 /* Material UI styling */
 const styles = theme => ({
@@ -36,7 +37,7 @@ const CoachingContactPage = props => (
           <p style={{ fontSize: '28px' }}>We&apos;re ready to partner with you.</p>
           <p style={{ fontSize: '22px' }}>Send us a message and we&apos;ll get in touch with you about your coaching options.</p>
         </div>
-        {/* <DissertationList /> */}
+        <ContactForm />
       </MuiThemeProvider>
     </div>
   </>
