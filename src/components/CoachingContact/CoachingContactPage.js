30219272122594b799924a9bc08a1ee4b73180d0
Hailee Miu <hailee.miu@gmail.com>
2019-01-31 09:09:30 -0600

    banner and heading

--- src/components/CoachingContact/CoachingContactPage.js
+++ src/components/CoachingContact/CoachingContactPage.js
@@ -1,15 +1,49 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { withStyles, createMuiTheme } from '@material-ui/core/styles';
+import { teal, red } from '@material-ui/core/colors';
+import CoachingBanner from './CoachingBanner';
 
-class CoachingContactPage extends Component {
-  state = {}
+/* Material UI styling */
+const styles = theme => ({
+  root: {
+    width: '50%',
+    maxWidth: 1400,
+    backgroundColor: theme.palette.background.paper,
+    padding: 20,
+    margin: 'auto',
+    fontFamily: 'Avenir',
+  },
+});
 
-  render() {
-    return (
-      <>
-        <div>coaching</div>
-      </>
-    );
-  }
-}
+const theme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: red,
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 
-export default CoachingContactPage;
+const CoachingContactPage = props => (
+  <>
+    <CoachingBanner />
+    <div className={props.classes.root}>
+      <MuiThemeProvider theme={theme}>
+        <div style={{ textAlign: 'center' }}>
+          <p style={{ fontSize: '28px' }}>We&apos;re ready to partner with you.</p>
+          <p style={{ fontSize: '22px' }}>Send us a message and we&apos;ll get in touch with you about your coaching options.</p>
+        </div>
+        {/* <DissertationList /> */}
+      </MuiThemeProvider>
+    </div>
+  </>
+);
+
+CoachingContactPage.propTypes = {
+  classes: PropTypes.shape().isRequired,
+};
+
+export default withStyles(styles)(CoachingContactPage);
